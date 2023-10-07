dc64dfb932dcce01aaf30a350f61bd4aa307d45e
blue <bluebone96@outlook.com>
2023-10-07 18:09:24 +0800

    add neogen plugin

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,8 @@
+-- neogen
+return {
+  "danymat/neogen",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  config = true,
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
+}
